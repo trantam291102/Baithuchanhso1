2cae2fa09eda0eddd4fb5af125a4a8b38d6fee22
trantam291102 <97502878+trantam291102@users.noreply.github.com>
2022-02-27 20:14:02 +0700

    Update and rename App4.java to App47.java

--- Baithuchanhjava/Baithuchanh1502/App47.java
+++ Baithuchanhjava/Baithuchanh1502/App47.java
@@ -1,6 +1,6 @@
 package Baithuchanh1502.src;
 import java.util.Scanner;
-public class App4 {
+public class App47 {
     private static Scanner scanner;
 
     public static void main(String[] args) {
@@ -8,10 +8,10 @@ public class App4 {
         try (Scanner sc = new Scanner(System.in)) {
         }
         do {
-            System.out.println("Nhap số nguyen a: ");
+            System.out.println("Nhập số nguyên a: ");
             a=scanner.nextInt();
             sum += a;
-        } while (sum<100); //tong lon hon 100 thi ket thuc
-        System.out.println("Tong các so nguyen vua nhap= "+sum);
+        } while (sum<100); //tổng lớn hơn 100 thì kết thúc
+        System.out.println("Tổng các số nguyên vừa nhập= "+sum);
     }
 }
