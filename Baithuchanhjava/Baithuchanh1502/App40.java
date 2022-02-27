ef62ea1df2a90e7556f50a955ede565dfe75c20e
trantam291102 <97502878+trantam291102@users.noreply.github.com>
2022-02-27 20:12:52 +0700

    Update and rename App3.java to App40.java

--- Baithuchanhjava/Baithuchanh1502/App40.java
+++ Baithuchanhjava/Baithuchanh1502/App40.java
@@ -1,23 +1,23 @@
 package Baithuchanh1502.src;
 import java.util.Scanner;
-public class App3 {
+public class App40 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int ngay = scanner.nextInt();
             switch(ngay) {
-                case 0:System.out.println("Thu hai");
+                case 0:System.out.println("Thứ hai");
                 break;
-                case 1:System.out.println("Thu ba");
+                case 1:System.out.println("Thứ ba");
                 break;
-                case 2:System.out.println("Thu tu");
+                case 2:System.out.println("Thứ tư");
                 break;
-                case 3:System.out.println("Thu nam");
+                case 3:System.out.println("Thứ năm");
                 break;
-                case 4:System.out.println("Thu sau");
+                case 4:System.out.println("Thứ sáu");
                 break;
-                case 5:System.out.println("Thu bay");
+                case 5:System.out.println("Thứ bảy");
                 break;
-                case 6:System.out.println("Chu nhat");
+                case 6:System.out.println("Chủ nhật");
                 break;
                 default:System.out.println("Đó không phải là ngày trong tuần");
             }
