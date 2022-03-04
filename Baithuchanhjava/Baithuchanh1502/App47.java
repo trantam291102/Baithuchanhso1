864ecf030ead717f9599958ee81261a9cb9ceca9
trantam291102 <97502878+trantam291102@users.noreply.github.com>
2022-03-04 17:20:02 +0700

    Update App47.java

--- Baithuchanhjava/Baithuchanh1502/App47.java
+++ Baithuchanhjava/Baithuchanh1502/App47.java
@@ -1,17 +1,17 @@
 package Baithuchanh1502.src;
 import java.util.Scanner;
 public class App47 {
-    private static Scanner scanner;
-
     public static void main(String[] args) {
-        int a, sum=0;
-        try (Scanner sc = new Scanner(System.in)) {
-        }
+        float sum = 0, a;
+    try (Scanner scanner = new Scanner(System.in)) {
         do {
-            System.out.println("Nhập số nguyên a: ");
-            a=scanner.nextInt();
+            System.out.println("Nhập vào số a:");
+            a = scanner.nextFloat();
             sum += a;
-        } while (sum<100); //tổng lớn hơn 100 thì kết thúc
-        System.out.println("Tổng các số nguyên vừa nhập= "+sum);
+            if (sum > 100)
+                break;
+        } while (a > 0);
+    }     
+    System.out.println("Tổng các số vừa nhập là " + sum);
     }
 }
